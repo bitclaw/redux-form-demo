8853db0a9fc7d8e23dc2e4b17d56dab96738bc02
Daniel C <bitclaw@gmail.com>
2018-07-24 03:15:55 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css';
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import Message from './components/Message'
+import Footer from './components/Footer'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Todo List</h1>
+                </header>
+                <Router>
+                    <div className="Todo-App">
+                        <Message />
+                        <TodoForm />
+                        <Route path='/:filter?' render={({match}) => (
+                            <TodoList filter = {match.params.filter} />
+                        )} />
+                        <Footer />
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App
